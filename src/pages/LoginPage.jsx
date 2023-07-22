1e0970e203ab1f51fc6bf7e480cd101fe884fefd
mhdwahyusiregar <siregarwahyu573@gmail.com>
2023-07-22 12:10:32 +0700

    make test pass 2

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -26,7 +26,7 @@ function LoginPage() {
           ,
           {' '}
           <br />
-          Through Open Space App.
+          Through Open Space App New.
         </h2>
 
         <LoginInput login={onLogin} />
